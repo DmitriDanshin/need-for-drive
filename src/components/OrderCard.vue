d6214f43e487f82572fcfc16aa852515dd25c8c6
DmitriDanshin <Dmitri.Danshin2017@gmail.com>
2021-08-07 21:13:03 +0300

    F-03, динамическое добавление предметов в карточку заказа

--- src/components/OrderCard.vue
+++ src/components/OrderCard.vue
@@ -5,34 +5,18 @@
   >
     <div class="order__title">Ваш заказ:</div>
     <div class="order__list">
-      <div class="order__list__item">
-        <div class="order__list__name">Пункт выдачи</div>
+      <div
+        class="order__list__item"
+        v-for="item in items"
+        :key="item.name"
+      >
+        <div class="order__list__name">{{ item.name }}</div>
         <div class="order__list__dots"></div>
-        <div class="order__list__value">Ульяновск, Нариманова 42</div>
-      </div>
-      <div class="order__list__item">
-        <div class="order__list__name">Модель</div>
-        <div class="order__list__dots"></div>
-        <div class="order__list__value">Hyndai, i30 N</div>
-      </div>
-      <div class="order__list__item">
-        <div class="order__list__name">Цвет</div>
-        <div class="order__list__dots"></div>
-        <div class="order__list__value">Голубой</div>
-      </div>
-      <div class="order__list__item">
-        <div class="order__list__name">Длительность аренды</div>
-        <div class="order__list__dots"></div>
-        <div class="order__list__value">1д 2ч</div>
-      </div>
-      <div class="order__list__item">
-        <div class="order__list__name">Полный бак</div>
-        <div class="order__list__dots"></div>
-        <div class="order__list__value">Да</div>
+        <div class="order__list__value">{{ item.value }}</div>
       </div>
     </div>
     <div class="order__price">
-      <div><span>Цена:</span> от 8 000 до 12 000 ₽</div>
+      <div><span>Цена:</span></div>
     </div>
     <div class="order__btn__wrapper">
       <button
@@ -88,6 +72,11 @@ export default {
       this.$emit("next-page");
     },
   },
+  computed: {
+    items() {
+      return this.$store.state.orderCardItems;
+    }
+  }
 };
 </script>
 
@@ -96,32 +85,38 @@ export default {
 @import "../scss/helpers";
 @import "../scss/fonts";
 @import "../scss/mixins";
+
 .order {
   height: 100%;
   border-left: 1px solid $gray-light;
   padding-left: 32px;
   padding-right: 64px;
   padding-top: 32px;
+
   &__mobile {
     display: none;
     margin-top: 8px;
     z-index: 5;
+
     &__button {
       @include button();
       width: 100%;
       border-radius: 0;
     }
   }
+
   &__price {
     margin-top: 32px;
     @include text();
     font-weight: 400;
     font-size: 16px;
     color: $black;
+
     span {
       font-weight: 500;
     }
   }
+
   &__title {
     @include text();
     font-weight: 500;
@@ -129,32 +124,39 @@ export default {
     text-align: right;
     color: $black;
   }
+
   &__list {
     margin-top: 26px;
+
     &__item {
       margin-top: 16px;
       display: flex;
       justify-content: space-between;
       align-items: flex-end;
     }
+
     &__name {
       @include text();
     }
+
     &__dots {
       border-bottom: $gray 1px dotted;
       width: 50%;
     }
+
     &__value {
       @include text();
       text-align: right;
       color: $gray;
     }
   }
+
   &__btn {
     margin-top: 32px;
     @include button();
     border-radius: 0;
     width: 100%;
+
     &__back {
       margin-top: 32px;
       @include button();
@@ -167,6 +169,7 @@ export default {
     }
   }
 }
+
 // mobile
 @media screen and (min-width: 320px) and (max-width: 767px) {
   .order {
@@ -176,6 +179,7 @@ export default {
     width: 100%;
     display: none;
     padding: 0;
+
     &-active {
       display: flex;
       flex-direction: column;
@@ -183,27 +187,33 @@ export default {
       height: 100%;
       visibility: visible;
     }
+
     &__btn {
       &__back {
         display: block;
       }
+
       &__wrapper {
         width: 100%;
       }
     }
+
     &__mobile {
       display: block;
       position: absolute;
       bottom: 10%;
       width: 100%;
     }
+
     &__title {
       text-align: left;
       padding-left: 12px;
     }
+
     &__price {
       padding-left: 12px;
     }
+
     &__list {
       margin-top: 8px;
       padding-right: 12px;
